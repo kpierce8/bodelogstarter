abf8ab4a97a52dc7d9794147edd80fa7e34a534b
Ken Pierce <kenpierce@Kens-MacBook-Pro.local>
2015-08-29 19:09:11 -0700

    trying other action url2

--- app.js
+++ app.js
@@ -153,7 +153,14 @@ http.createServer(function (req, res) {
       createWebpage(req, res);
       addExerciseData(req, res);
       break;
-
+      case 'POST':
+        req.setEncoding('utf8');
+        req.on('data', function(chunk) {
+            console.log(chunk);
+            //processSet(chunk);
+            });
+        res.end('ended process chunk = ');
+      break;
       default:
         badRequest(res); //added route logic from Node in Action page 88 to fill out app
       }
@@ -265,6 +272,6 @@ var html4 = '<h2> Queried (name.last = "Doe", age >64) Documents in MonogoDB dat
 var html5 = '</code></pre> <br\> <i>';
 var html6 = ' documents. </i> <br\> <br\> \
 <br\> <br\> <center><i> Demo code available at <a href="http://github.com/mongolab/hello-mongoose">github.com</a> </i></center></b><a href="../sets/">Add Set</a>';
-var html7 = '<form method="post" action="/sets"><p><input type="text" name="exercise"/> Exercise</p><p><input type="text" name="reps"/> Reps</p><p><input type="text" name="resistance"/> Resistance</p><p><input type="submit" value="Add Set"/></p></form>';
+var html7 = '<form method="post" action="/"><p><input type="text" name="exercise"/> Exercise</p><p><input type="text" name="reps"/> Reps</p><p><input type="text" name="resistance"/> Resistance</p><p><input type="submit" value="Add Set"/></p></form>';
 
 
